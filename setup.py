184a27c09a1699ce83b631785d0ae03b67f4aed1
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-06-05 01:36:55 +1000

    add notes

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="pydd",
     version="1.0",
     description="mv (using dd followed by rm) with a progressbar",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/avivajpeyi/pydd",
     author="Avi Vajpeyi",
     author_email="avi.vajpeyi@gmail.com",
@@ -12,4 +18,8 @@ setup(
     zip_safe=False,
     install_requires=["tqdm>=4.32.1"],
     entry_points={"console_scripts": ["pydd=pydd.pydd:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
